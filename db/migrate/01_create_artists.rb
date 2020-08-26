0b829e08529194d2f014ef105939a1d95acdd0c2
Ryna Truong <rynatruong@outlook.com>
2020-08-26 16:14:23 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -7,8 +7,12 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def down
   end
   
-  def change
-  create_table :artists do |t|
+ def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
-end
 end
