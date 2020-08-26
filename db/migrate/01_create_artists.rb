f510411a2c5144f3a8f92c9b432121c0cd984dc8
Ryna Truong <rynatruong@outlook.com>
2020-08-26 16:06:19 +0000

    Automatically backed up by Learn

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+mechanics-of-migrations-v-000/
+  config/
+    environment.rb
+  db/
+    migrate/
+      01_create_artists.rb
+  spec/
+    artist_spec.rb
+    spec_helper.rb
+  .gitignore
+  .learn
+  .rspec
+  artist.rb
+  CONTRIBUTING.md
+  Gemfile
+  Gemfile.lock
+  LICENSE.md
+  Rakefile
+  README.md
